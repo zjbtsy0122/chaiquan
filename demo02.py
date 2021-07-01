982e9fb2012eb967508814075b46c94ea24c580b
ZhuJianbo <zjbzjb0122@163.com>
2021-07-01 15:31:25 +0800

    0701下午

--- demo02.py
+++ demo02.py
@@ -8,14 +8,41 @@
 # else:
 #     print("b更大")
 
+#if
+#if else
+#if elif else
 
-a = input("请输入：")
-if a in "0123456789":
-    a = int(a)
-else:
-    print("请输入正确的年龄！")
-    exit()
-if a > 5:
-    print("大")
-else :
-    print("小")
+
+# a = input("请输入：")
+# if a in "0123456789":
+#     a = int(a)
+# else:
+#     print("请输入正确的年龄！")
+#     exit()
+# if a > 5:
+#     print("大")
+# else :
+#     print("小")
+
+# a =1 
+
+
+# while a < 10 :
+#     print("哈哈哈哈",a)
+
+#     a = a + 1
+
+highchengji = {}
+lowchengji = {}
+
+student = ["张三","李四","赵四","王五","溜溜","lily","haha","zhuzhu","xiaoyun","hihi"]
+a = 0 
+while a < len(student):
+    chengji = int(input("请输入"+student[a]+"的成绩："))
+    if chengji >= 60 :
+        highchengji[student[a]] = chengji
+    else :
+        lowchengji[student[a]] = chengji 
+    a = a + 1
+
+print(highchengji,lowchengji)
