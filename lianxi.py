7bae4f11a0c39dae7e27c01bd90aa3fbc44c9f16
ZhuJianbo <zjbzjb0122@163.com>
2021-07-05 10:15:36 +0800

    070502

--- lianxi.py
+++ lianxi.py
@@ -5,4 +5,26 @@
 # 用户输入账号和密码，要求账号长度是5-8位，密码6-12位，并且账号必须小写开头。
 # 储到字典中，{username:password}
 
+# 答案1
+# light = {"红灯":30 ,"绿灯":35 ,"黄灯":3}
+# while True:
+#     for i in light :
+#         for j in range(light[i]):
+#             print(i,"倒计时还有",light[i]-j,"秒")
+
+# 答案2
+
+username = input("请输入账号：")
+password = input("请输入密码：")
+
+if len(username) >=5 and len(username)<=8:
+    if username[0] in "qwertyuiopasdfghjklzxcvbnm":
+        if len(password) >=6 and len(password) <= 12:
+            print("注册成功！",{username:password})
+        else:
+            print("密码必须是6-12位") 
+    else :
+        print("账号的首字母必须小写开头")
+else :
+    print("账号长度不符合规范，请输入长度是5-8位")
 
